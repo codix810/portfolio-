ffef608935c025cea64e43dc985ce752be42bc70
CodiX 🧑‍💻 <codix810@gmail.com>
2025-06-25 11:22:15 +0300

    sendMessage.js

--- api/sendMessage.js
+++ api/sendMessage.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGO_URI;
-console.log("📦 MONGO_URI = ", uri); // مهم
+const options = {};
 
 let client;
 let clientPromise;
@@ -10,8 +10,6 @@ if (!global._mongoClientPromise) {
   client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    tls: true,
-    tlsAllowInvalidCertificates: true,
   });
   global._mongoClientPromise = client.connect();
 }
@@ -32,17 +30,16 @@ export default async function handler(req, res) {
     const client = await clientPromise;
     const db = client.db("houdaDB");
 
-    const result = await db.collection("messages").insertOne({
+    await db.collection("messages").insertOne({
       name,
       email,
       message,
       createdAt: new Date(),
     });
 
-    console.log("✅ Insert result:", result.insertedId);
     return res.status(200).json({ message: "Message stored successfully!" });
   } catch (error) {
-    console.error("🔥🔥🔥 REAL ERROR:", error);
-    return res.status(500).json({ message: error.message });
+    console.error("🔥 REAL ERROR:", error);
+    return res.status(500).send("A server error has occurred");
   }
 }
